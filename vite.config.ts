5b6216bc83149a24ebcddf3a021c7721538f7a2b
Zachary Robinson <me@zsrobinson.com>
2026-08-18 17:40:12 -0400

    scaffold with correct vite starter

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,35 @@
-import { defineConfig } from "vite-plus";
+import { defineConfig, lazyPlugins } from "vite-plus";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
 
+// https://vite.dev/config/
 export default defineConfig({
   staged: {
     "*": "vp check --fix",
   },
   fmt: {},
   lint: {
-    jsPlugins: [{ name: "vite-plus", specifier: "vite-plus/oxlint-plugin" }],
-    rules: { "vite-plus/prefer-vite-plus-imports": "error" },
-    options: { typeAware: true, typeCheck: true },
+    plugins: ["react", "typescript", "oxc"],
+    rules: {
+      "react/rules-of-hooks": "error",
+      "react/only-export-components": [
+        "warn",
+        {
+          allowConstantExport: true,
+        },
+      ],
+      "vite-plus/prefer-vite-plus-imports": "error",
+    },
+    options: {
+      typeAware: true,
+      typeCheck: true,
+    },
+    jsPlugins: [
+      {
+        name: "vite-plus",
+        specifier: "vite-plus/oxlint-plugin",
+      },
+    ],
   },
+  plugins: lazyPlugins(() => [react(), babel({ presets: [reactCompilerPreset()] })]),
 });
